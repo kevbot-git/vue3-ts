3a68c31a13394a26d02339ff95de3ba06cbfe889
Kevin Menhinick <kamenhinick@gmail.com>
2021-08-28 21:51:21 +1200

    Configure basic linting

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    extraFileExtensions: ['.vue']
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
+};
